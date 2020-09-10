f2a86ea22e2b4a295d6c327aad8200b8a1b7770d
fernandoruralsmart <fernando.marcato@ruralsmart.com.br>
2020-09-09 21:43:45 -0300

    bkp

---
+++ flutter_book/lib/notes/notesModel.dart
@@ -0,0 +1,13 @@
+
+import "../BaseModel.dart";
+
+class Note {
+	int id;
+	String title;
+	String content;
+	String color;
+
+	String toString(){
+		return "{ id=$id, title=$title, content=$content, color=$color }";
+	}
+}
