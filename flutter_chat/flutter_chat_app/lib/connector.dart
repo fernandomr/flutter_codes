b0d0445c0f10a474397f317f6e2d843be76862a6
fernandoruralsmart <fernando.marcato@ruralsmart.com.br>
2020-10-19 23:49:22 -0300

    bkp

--- flutter_chat/flutter_chat_app/lib/connector.dart
+++ flutter_chat/flutter_chat_app/lib/connector.dart
@@ -1,2 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/flutterChatModel.dart';
 import 'package:flutter_socket_io/flutter_socket_io.dart';
 import 'package:flutter_socket_io/socket_io_manager.dart';
+
+String serverURL = "http://192.168.1.2";
+SocketIO _io;
+
+void showPleaseWait(){
+  showDialog(
+    context: model.rootBuildContext,
+    barrierDismissible: false,
+    builder: (BuildContext context){
+      return Dialog(
+        child: Container(width: 150, height: 150,
+          alignment: AlignmentDirectional.center,
+          decoration: BoxDecoration(color: Colors.blue[200]),
+        ),
+      );
+    }
+  );
+}
