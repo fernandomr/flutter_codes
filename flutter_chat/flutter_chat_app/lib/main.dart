eb3d987d8c00c956abebd8d7d1b62bc79f8a07ac
fernandoruralsmart <fernando.marcato@ruralsmart.com.br>
2020-10-27 23:58:11 -0300

    bkp

--- flutter_chat/flutter_chat_app/lib/main.dart
+++ flutter_chat/flutter_chat_app/lib/main.dart
@@ -1,29 +1,34 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_app/flutterChatModel.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
+  void startUp() async {
+    Directory docsDir = await getApplicationDocumentsDirectory();
+    model.docsDir = docsDir;
+
+    var credentialsFile = File(join(model.docsDir.path, "credentials"));
+    var exists = await credentialsFile.exists();
+
+    var credentials;
+    if (exists){
+      credentials = credentialsFile.readAsString();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    startUp();
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
