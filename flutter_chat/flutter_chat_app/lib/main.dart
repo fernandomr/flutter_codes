30e2dda4ad87708b2a39d79082baca383bc69424
fernandoruralsmart <fernando.marcato@ruralsmart.com.br>
2020-10-28 23:03:10 -0300

    bkp

--- flutter_chat/flutter_chat_app/lib/main.dart
+++ flutter_chat/flutter_chat_app/lib/main.dart
@@ -10,38 +10,15 @@ void main() {
 
 class MyApp extends StatelessWidget {
 
-  void startUp() async {
-    Directory docsDir = await getApplicationDocumentsDirectory();
-    model.docsDir = docsDir;
-
-    var credentialsFile = File(join(model.docsDir.path, "credentials"));
-    var exists = await credentialsFile.exists();
-
-    var credentials;
-    if (exists){
-      credentials = credentialsFile.readAsString();
-      List credParts = credentials.split("============");
-      LoginDialog().validateWithStoredCredentials(credParts[0], credParts[1]);
-    } else {
-      await showDialog(context: model.rootBuildContext,
-        barrierDismissible: false,
-        builder: (BuildContext dialogContext) {
-        return LoginDialog();
-        }
-      );
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    startUp();
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Chat',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Chat home page'),
     );
   }
 }
@@ -56,41 +33,46 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  void startUp() async {
+    Directory docsDir = await getApplicationDocumentsDirectory();
+    model.docsDir = docsDir;
+
+    var credentialsFile = File(join(model.docsDir.path, "credentials"));
+    var exists = await credentialsFile.exists();
+
+    var credentials;
+    if (exists){
+      credentials = credentialsFile.readAsString();
+      List credParts = credentials.split("============");
+      LoginDialog().validateWithStoredCredentials(credParts[0], credParts[1]);
+    } else {
+      await showDialog(context: model.rootBuildContext,
+          barrierDismissible: false,
+          builder: (BuildContext dialogContext) {
+            return LoginDialog();
+          }
+      );
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    startUp();
 
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
       body: Center(
-
         child: Column(
-
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              'App being built',
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ),
     );
   }
