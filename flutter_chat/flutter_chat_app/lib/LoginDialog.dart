9a60995305eedaa9d2f393b8d599c7ec39ce5500
fernandoruralsmart <fernando.marcato@ruralsmart.com.br>
2020-10-29 23:15:39 -0300

    bkp

--- flutter_chat/flutter_chat_app/lib/LoginDialog.dart
+++ flutter_chat/flutter_chat_app/lib/LoginDialog.dart
@@ -16,7 +16,58 @@ class LoginDialog extends StatelessWidget {
         model: model,
         child: ScopedModelDescendant<FlutterChatModel>(
           builder: (BuildContext context, Widget child, FlutterChatModel inModel){
-            return ;
+            return AlertDialog(
+              content: Container(
+                height: 220,
+                child: Form(
+                  key: _loginFormKey,
+                  child: Column(
+                      children: [
+                        Text(
+                          "Enter a username and password to register with the server",
+                          textAlign: TextAlign.center,
+                          // TODO fontSize: 18,
+                          style: TextStyle(
+                              color: Theme.of(model.rootBuildContext).accentColor
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        TextFormField(
+                          validator: (String value){
+                            if (value.length == 0 || value.length > 10){
+                              return "Please enter a username between 1 and 10 characters";
+                            }
+                            return null;
+                          },
+                          onSaved: (String value){
+                            _userName = value;
+                          },
+                          decoration: InputDecoration(
+                            hintText: "Username",
+                            labelText: "Username"
+                          ),
+                        ),
+                        TextFormField(
+                          obscureText: true,
+                          validator: (String value){
+                            if (value.length == 0){
+                              return "Please enter a password";
+                            }
+                            return null;
+                          },
+                          onSaved: (String value){
+                            _password = value;
+                          },
+                          decoration: InputDecoration(
+                              hintText: "Password",
+                              labelText: "Password"
+                          ),
+                        ),
+                      ]
+                  ),
+                )
+              )
+            );
           },
         ),
     );
