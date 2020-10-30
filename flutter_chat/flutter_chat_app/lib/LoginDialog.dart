18a6bea2508375c04881784fc04bbe3b8d32e8cf
fernandoruralsmart <fernando.marcato@ruralsmart.com.br>
2020-10-29 23:35:37 -0300

    bkp

--- flutter_chat/flutter_chat_app/lib/LoginDialog.dart
+++ flutter_chat/flutter_chat_app/lib/LoginDialog.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_chat_app/flutterChatModel.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:scoped_model/scoped_model.dart';
+import 'connector.dart' as connector;
 
 class LoginDialog extends StatelessWidget {
   static final GlobalKey<FormState> _loginFormKey = new GlobalKey<FormState>();
@@ -66,7 +67,43 @@ class LoginDialog extends StatelessWidget {
                       ]
                   ),
                 )
-              )
+              ),
+              actions: [
+                FlatButton(
+                  child: Text("Log in"),
+                  onPressed: () {
+                    if (_loginFormKey.currentState.validate()){
+                      _loginFormKey.currentState.save();
+                      connector.connectToServer(model.rootBuildContext, () {
+                        connector.validate(_userName, _password,
+                            (inStatus) async {
+                              if (inStatus == "ok"){
+                                model.setUserName(_userName);
+                                Navigator.of(model.rootBuildContext).pop();
+                                model.setGreetings("Welcome, $_userName");
+                              } else if (inStatus == "fail") {
+                                Scaffold.of(model.rootBuildContext)
+                                    .showSnackBar(
+                                  SnackBar(
+                                    backgroundColor: Colors.red,
+                                    duration: Duration(seconds: 2),
+                                    content: Text("Sorry, username name is already taken"),
+                                  )
+                                );
+                              } else if (inStatus == "created"){
+                                var credentialsFile = File(join(model.docsDir.path, "credentials"));
+                                await credentialsFile.writeAsString("$_userName============$_password");
+                                model.setUserName(_userName);
+                                Navigator.of(model.rootBuildContext).pop();
+                                model.setGreetings("Welcome to the server, $_userName");
+                              }
+                            }
+                        );
+                      });
+                    }
+                  },
+                ),
+              ],
             );
           },
         ),
